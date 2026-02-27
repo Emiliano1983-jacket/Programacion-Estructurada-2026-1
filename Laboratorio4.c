6faa2a77b673b250540fd30bdf5162591e2d7e91
Emiliano1983-jacket <ramon.felix.garcia@uabc.edu.mx>
2026-02-27 14:52:24 -0800

    Create Laboratorio4.c

---
+++ Laboratorio4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void ResultadoEncuestaVacunacion(int Datos[],int Opciones[]) 
+{ int cnt;
+  int TOTAL_MAX = 500;
+  int TOTAL_OPCIONES = 5;
+for (cnt = 0 ; cnt < TOTAL_OPCIONES; c++){opciones[cnt]=0};
+for (cnt = 0 ; cnt < TOTAL_MAX; cnt++){
+switch(Datos[cnt]){
+  case 1: opciones[0]++;
+          break; 
+  case 2: opciones[1]++;
+          break;
+  case 3: opciones[2]++;
+          break;
+  case 4: opciones[3]++;
+          break;
+  case 5: opciones[4]++;
+          break;
+          default;
+printf("Opcion invalida %d",Datos[cnt]);
+    }
+  }
+}
+  
