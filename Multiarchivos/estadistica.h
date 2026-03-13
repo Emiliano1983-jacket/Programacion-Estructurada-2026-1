9f66ea1b371bae5ef5a9a29ccf54c00db80f4970
Emiliano1983-jacket <ramon.felix.garcia@uabc.edu.mx>
2026-03-13 22:00:52 +0000

    commit

--- Multiarchivos/estadistica.h
+++ Multiarchivos/estadistica.h
@@ -1,9 +1,6 @@
-#ifndef ESTADISTICA_H
-#define ESTADISTICA_H
 float calcularPromedio(float calificaciones[], int n);
 float obtenerCalificacionMaxima(float calificaciones[], int n);
 float obtenerMenor(float calificaciones[], int n);
 int contarAprobados(float calificaciones[], int n);
 int contarReprobados(float calificaciones[], int n);
 
-#endif 
