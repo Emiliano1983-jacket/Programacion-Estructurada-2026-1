ab52f50de908c5b4e1bc630fb6694e828888f409
Emiliano1983-jacket <ramon.felix.garcia@uabc.edu.mx>
2026-03-23 22:05:07 +0000

    commit

--- Multiarchivos/complejo.c
+++ Multiarchivos/complejo.c
@@ -8,7 +8,7 @@ struct Complejo leerComplejo(){
     return c;
 }
        
-   void mostrarComplejo(struct Complejo c){
+void mostrarComplejo(struct Complejo c){
     printf("Número complejo: %.2lf + %.2lfi\n", c.real, c.imaginaria);
 }
                     
@@ -18,9 +18,11 @@ struct Complejo sumar(struct Complejo c1, struct Complejo c2){
     resultado.imaginaria = c1.imaginaria + c2.imaginaria;
     return resultado;
 }
-                        
-        
-                  
 
-        struct Complejo multiplicar(struct Complejo c1, struct Complejo c2): 
+struct Complejo multiplicar(struct Complejo c1, struct Complejo c2){
+    struct Complejo multiplicado;
+    multiplicado.real = (c1.real * c2.real) - (c1.imaginaria * c2.imaginaria);
+    multiplicado.imaginaria = (c1.real * c2.imaginaria) + (c1.imaginaria * c2.real);
+    return multiplicado;
+}
                     
--- Multiarchivos/lab8.c
+++ Multiarchivos/lab8.c
@@ -1,3 +1,22 @@
 //AQUI VA EL MAIN
 #include "complejo.h"
-#include <stdio.h>
+#include <stdio.h> 
+int main(){
+    struct Complejo c1 = leerComplejo();
+    struct Complejo c2 = leerComplejo();
+
+    printf("Primer número complejo:\n");
+    mostrarComplejo(c1);
+    printf("Segundo número complejo:\n");
+    mostrarComplejo(c2);
+
+    struct Complejo suma = sumar(c1, c2);
+    struct Complejo producto = multiplicar(c1, c2);
+
+    printf("Suma de los números complejos:\n");
+    mostrarComplejo(suma);
+    printf("Producto de los números complejos:\n");
+    mostrarComplejo(producto);
+
+    return 0;
+}
